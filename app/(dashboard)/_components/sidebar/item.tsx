0d413a3f6168f8fe66a76d63dbcc855c7af9c60d
sillymk18 <sillymk18@gmail.com>
2025-09-03 05:51:15 +0800

    feat: Add dashboard layout and sidebar components with organization creation dialog
    - Implemented a new sidebar component with a button to create organizations.
    - Created a dashboard layout that integrates the sidebar and navbar.
    - Added a basic dashboard page structure.
    - Introduced utility components for dialog and button functionalities.
    - Integrated loading component for authentication states.
    - Set up Convex client provider for managing authentication and state.
    - Added necessary configurations and generated files for Convex integration.
    - Included a logo SVG for branding purposes.

---
+++ app/(dashboard)/_components/sidebar/item.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import Image from"next/image";
+
+import {
+    useOrganization,
+    useOrganizationList,
+} from "@clerk/nextjs";
+
+import { cn } from"@/lib/utils";
---
+++ providers/convex-client-provider.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { Loading } from"@/components/auth/loading";
+
+import {
+    AuthLoading,
+    Authenticated,
+    ConvexReactClient,
+    Unauthenticated
+} from"convex/react";
+import { Children } from "react";
+
+interface ConvexClientProviderProps {
+    children: React.ReactNode;
+};
+
+const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
+
+const convex = new ConvexReactClient(convexUrl);
+
+export const ConvexClientProvider = ({
+    children,
+}: ConvexClientProviderProps) => {
+    return (
+        <ClerkProvider>
+            <ConvexProviderWithClerk useAuth={useAuth} client= {convex}>
+                <Authenticated>
+                    {children}
+                </Authenticated>
+                <Unauthenticated>
+                    {children}
+                </Unauthenticated>
+                <AuthLoading>
+                    <Loading />
+                </AuthLoading>
+            
+            </ConvexProviderWithClerk>
+        </ClerkProvider>
+    );
+
+};
